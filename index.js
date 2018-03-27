e3120e41464f4e61b94574b3585dc3ea735041c8
wanls4583 <409051987@qq.com>
2018-03-27 22:07:57 +0800

    add file

---
+++ index.js
@@ -0,0 +1,24 @@
+Class Util {
+
+    static I64BIT_TABLE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-'.split('');
+    
+    static hash(input) {
+
+        let hash = 5381;
+        let i = input.length - 1;
+        if (typeof input == 'string') {
+            for (; i > -1; i--)
+                hash += (hash << 5) + input.charCodeAt(i);
+        } else {
+            for (; i > -1; i--)
+                hash += (hash << 5) + input[i];
+        }
+        let value = hash & 0x7FFFFFFF;
+        let retValue = '';
+        do {
+            retValue += Util.I64BIT_TABLE[value & 0x3F];
+        }
+        while (value >>= 6);
+        return retValue;
+    }
+}
